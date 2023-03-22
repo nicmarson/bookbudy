d1859b1b316acf6fb00cb21a9b1bab6a5be4ef3c
Nicholas Marson <nicmarson@gmail.com>
2023-03-22 16:21:11 -0700

    fixed error

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ function fetchFeaturedBooks() {
 
 function fetchRecommendedBooks() {
   // fetch recommended books data from API
-  // parse data
+  // parse data and populate recommendedBooksList using DOM manipulation
+}
+
+function fetchInfluencers() {
+  // fetch influencers data from API
+  // parse data and populate influencersList using DOM manipulation
+}
+
+function fetchBookLists() {
+  // fetch book lists data from API
+  // parse data and populate bookListsList using DOM manipulation
+}
+
+fetchFeaturedBooks();
+fetchRecommendedBooks();
+fetchInfluencers();
+fetchBookLists();
