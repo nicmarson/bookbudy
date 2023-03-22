30c416b11462c3ad72afc66991405fbf71cb4d0b
Nic Marson <nicmarson@gmail.com>
2023-03-22 09:21:16 -0700

    new node site

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,70 +1,27 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    project: './tsconfig.json',
-    extraFileExtensions: ['.vue'],
-  },
   env: {
     browser: true,
-    jest: true,
+    commonjs: true,
+    es2021: true,
     node: true,
   },
   extends: [
-    'airbnb-typescript/base',
-    'plugin:vue-a11y/recommended',
+    'standard-with-typescript',
     'plugin:@typescript-eslint/recommended',
-    'plugin:vue/strongly-recommended',
     'prettier',
-    'prettier/vue',
-    'prettier/@typescript-eslint',
   ],
-  plugins: ['@typescript-eslint', 'vue-a11y', 'prettier', 'vue'],
-  // add your custom rules here
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+  },
   rules: {
-    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'es5', printWidth: 100 }],
-    'import/extensions': [
-      'error',
-      'always',
-      {
-        js: 'never',
-        ts: 'never',
-      },
-    ],
-    'no-console': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    'import/no-dynamic-require': 0,
-    'global-require': 0,
-    'no-underscore-dangle': 0,
-    'class-methods-use-this': 0,
-    'vue/max-attributes-per-line': 'off',
-    'vue/component-name-in-template-casing': [1, 'kebab-case'],
+    semi: 'off',
+    '@typescript-eslint/semi': 'off',
   },
-  settings: {
-    'import/core-modules': [
-      '@nuxt/config',
-      '@nuxt/vue-app',
-      '@nuxt/types',
-      'purgecss-webpack-plugin',
-      'vue',
-      'vuex',
-      'vue-meta',
-      'vue-server-renderer',
-      'vue-router',
-    ],
-    'import/resolver': {
-      webpack: {
-        config: {
-          resolve: {
-            extensions: ['.js', '.json', '.ts', '.vue'],
-            alias: {
-              '~': __dirname + '/app',
-              '@': __dirname + '/app',
-            },
-          },
-        },
-      },
-    },
-  },
-};
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+}
