f064397c3884093c6b42b107010321482648948e
Nicholas Marson <nicmarson@gmail.com>
2023-05-03 19:45:16 -0700

    removed next

--- pages/api/newsletter.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const email = req.body;
-
-  if (!email || !email.length) {
-    res.status(200).json({ error: "Please enter a email address" });
-  }
-}
